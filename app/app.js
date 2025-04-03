ee485e35a97384f79e8b90b337ab0d14432eede5
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2025-04-03 10:14:29 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,58 @@
+const classes = {
+    dir: "folder",
+    file: "file",
+    info: "info",
+    accordion: "accordion",
+    open: "open",
+    closed: "closed",
+    list: "list",
+    display: "display",
+    hide: "hide"
+}
+
+function generate_list_elements(list, cdir = "", recursive = false) {
+    const list_elements = document.createElement("ul");
+    let list_display_state, accordion_state;
+    if (recursive) {
+        list_display_state = classes.hide;
+        accordion_state = classes.closed;
+    } else {
+        list_display_state = classes.display;
+        accordion_state = classes.open;
+    }
+    list_elements.classList.add(classes.list, list_display_state);
+    let list_item, item_content, item_class, item_id, item_path;
+    for (const item in list) {
+        list_item = document.createElement("li");
+        if (typeof list[item] === "string") {
+            item_content = list[item];
+            item_class = classes.file;
+        } else {
+            item_content = item;
+            item_class = classes.dir;
+        }
+        const separator = "-";
+        item_id = `${cdir}-${item_content}`.replaceAll(".", separator);
+        item_path = `/${cdir.replaceAll(separator, "/")}/${item_content}`;
+        list_item.innerHTML = `<div id="${item_id}" class="${item_class}"><div class="${classes.info}"><a href="${item_path}">${item_content}</a></div></div>`;
+        if (typeof list[item] === "object" && Object.keys(list[item]).length > 0) {
+            list_item.querySelector(`.${item_class}`).innerHTML += `<button class="${classes.accordion} ${accordion_state}" type="button" onclick="toggle_list_display(this)"><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 12H18M12 6V18" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>`;
+            list_item.appendChild(generate_list_elements(list[item], item_id, true));
+        }
+        list_elements.appendChild(list_item);
+    }
+    return list_elements;
+}
+
+function toggle_list_display(source) {
+    const dir_content = source.parentNode.parentNode.querySelector(`.${classes.list}`);
+    if (source.classList.contains(classes.open) && dir_content.classList.contains(classes.display)) {
+        source.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 12H18M12 6V18" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+        source.classList.replace(classes.open, classes.closed);
+        dir_content.classList.replace(classes.display, classes.hide);
+    } else {
+        source.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 12L18 12" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
+        source.classList.replace(classes.closed, classes.open);
+        dir_content.classList.replace(classes.hide, classes.display);
+    }
+}
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+        $target = 'content';
+        $title = 'Directory: ' . substr(getcwd(), 1) . '/' . $target;
+        //$host = strtoupper($_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']);
+        $host = gethostbyname(gethostname());
+
+        function list_dir_contents($dir) {
+            $list = [];
+            $cdir = scandir($dir);
+            foreach ($cdir as $subdir) {
+                if (substr($subdir, 0, 1) != '.') {
+                    $subdirpath = $dir . DIRECTORY_SEPARATOR . $subdir;
+                    if (is_dir($subdirpath)) {
+                        $list[$subdir] = list_dir_contents($subdirpath);
+                    } else {
+                        $list[] = $subdir;
+                    }
+                }
+            }
+            return $list;
+        }
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
+    <link href="style/style.css" rel="stylesheet">
+</head>
+<body>
+    <div id="container">
+        <header id="header">
+            <div id="header-content">
+                <h1 id="title"><?= $title?></h1>
+                <p>Running at: <?= $host?></p>
+            </div>
+        </header>
+        <main id="main"></main>
+        <footer id="footer">
+            <p>&copy; 2025 &middot; Kasper Kouhia</p>
+        </footer>
+    </div>
+    <script src="app/app.js"></script>
+    <script>document.querySelector("#main").appendChild(generate_list_elements(<?= json_encode(list_dir_contents($target))?>, "<?= $target?>"));</script>
+</body>
+</html>
